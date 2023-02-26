0d43009855c767fd21d0bf15338e2feb1ccd2531
Fabricyo Barreto <bass.barreto@gmail.com>
2023-02-26 19:55:32 -0300

    chore: config enviroment variable of API

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  await dotenv.load(fileName: '.env');
+
   runApp(MaterialApp(
     home: Container(),
   ));
+}
+
+Future<Map> getCurrencies() async {
+  var endPoint = dotenv.env['API_ENDPOINT_CURRENCY'];
+  var url = Uri.parse(endPoint!);
+  var response = await http.get(url);
+  return json.decode(response.body);
 }
